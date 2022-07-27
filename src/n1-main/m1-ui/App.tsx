c1447fea9f28fc670b7317cda3f26f1642797d76
Angor78 <angor78@gmail.com>
2022-07-27 11:03:26 +0300

    add first structurization

---
+++ src/n1-main/m1-ui/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/*hr,prov*/}
+      <>
+        {/*<Header/>*/}
+
+      </>
+    </div>
+  );
+}
+
+export default App;
